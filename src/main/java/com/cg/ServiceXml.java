3146c8ade5eb9cf31f0514a1f8600f18968ac8c1
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2024-01-21 03:43:11 +0700

    udah bisa download

--- src/main/java/com/cg/ServiceXml.java
+++ src/main/java/com/cg/ServiceXml.java
@@ -1,32 +1,41 @@
 package com.cg;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.FileSystemResource;
 
+import org.springframework.core.io.ByteArrayResource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-
 
 @Controller
 public class ServiceXml {
 
     @Autowired
     private Environment env;
-    
+
     private static class DataEntry {
         private String nama;
         private String key;
@@ -56,12 +65,12 @@ public class ServiceXml {
     @GetMapping("/")
     public String index(Model model) {
         dataMap.clear();
-        model.addAttribute("dataMap",dataMap);
+        model.addAttribute("dataMap", dataMap);
         return "index";
     }
 
     @PostMapping("/saveData")
-    public ResponseEntity<Object> saveData( @RequestParam("nama") String nama,
+    public ResponseEntity<Object> saveData(@RequestParam("nama") String nama,
             @RequestParam("key") String key,
             @RequestParam("data") String[] data) {
 
@@ -79,27 +88,37 @@ public class ServiceXml {
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
-    @PostMapping("/saveXml")
-    public ResponseEntity<Map> saveXml() {
-        Map data = new HashMap<>();
-        saveXmlToFile(dataMap);
-        data.put("icon", "success");
-        data.put("message", "File XML Berhasil Dibuat.");
-        return new ResponseEntity<>(data, HttpStatus.OK);
+    @RequestMapping(path = "/saveXml", method = RequestMethod.GET)
+    public ResponseEntity<ByteArrayResource> downloadLargeFile() throws IOException {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        String xmlResult = generateXmlFromMap(dataMap);
+        String nm = dataMap.get(0).getNama();
+        String nama = nm.replace(" ", "_");
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(
+                new OutputStreamWriter(byteArrayOutputStream, StandardCharsets.UTF_8))) {
+            writer.write(xmlResult);
+        }
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + nama + ".xml\"");
+        ByteArrayResource byteArrayResource = new ByteArrayResource(byteArrayOutputStream.toByteArray());
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(byteArrayResource);
     }
 
-    private void saveXmlToFile(List<DataEntry> dataMap2) {
-        String xmlResult = generateXmlFromMap(dataMap2);
-        // String filePath = "C:/Users/fadhl/OneDrive/Documents/result.xml";
-        String nm = dataMap2.get(0).getNama();
-        String nama = nm.replace(" ", "_");
-        try (FileWriter fileWriter = new FileWriter(env.getProperty("URL.FILE_IN") + nama + ".xml")) {
-            fileWriter.write(xmlResult);
-            System.out.println("XML saved to file: " + env.getProperty("URL.FILE_IN") + nama + ".xml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    // private void saveXmlToFile(List<DataEntry> dataMap2) {
+    //     String xmlResult = generateXmlFromMap(dataMap2);
+    //     // String filePath = "C:/Users/fadhl/OneDrive/Documents/result.xml";
+    //     String nm = dataMap2.get(0).getNama();
+    //     String nama = nm.replace(" ", "_");
+    //     try (FileWriter fileWriter = new FileWriter(env.getProperty("URL.FILE_IN") + nama + ".xml")) {
+    //         fileWriter.write(xmlResult);
+    //         System.out.println("XML saved to file: " + env.getProperty("URL.FILE_IN") + nama + ".xml");
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
     private String generateXmlFromMap(List<DataEntry> dataMap2) {
         StringBuilder xmlBuilder = new StringBuilder();
@@ -112,15 +131,20 @@ public class ServiceXml {
         xmlBuilder.append("<entry name=\"show\">\n");
         xmlBuilder.append("<entry name=\"{40F77AF2-AC11-4596-A12B-CA0F3BCE3E8C}\">\n");
 
-        for (int i = 1; i <= dataMap2.size() ; i++) {
+        for (int i = 1; i <= dataMap2.size(); i++) {
 
             Integer elementName = tigak + i;
             String kunci = dataMap2.get(i - 1).getKey();
             String[] data = dataMap2.get(i - 1).getData();
-            xmlBuilder.append("<element loaded=\"0.00\" description=\"\" available=\"1.00\" layer=\"\" templatedescription=\"\" take_count=\"0\" showautodescription=\"True\" name=\"" + elementName + "\">\n");
-            xmlBuilder.append("<ref name=\"master_template\">/storage/shows/{40F77AF2-AC11-4596-A12B-CA0F3BCE3E8C}/mastertemplates/" + kunci + "</ref>\n").append("\n");
+            xmlBuilder.append(
+                    "<element loaded=\"0.00\" description=\"\" available=\"1.00\" layer=\"\" templatedescription=\"\" take_count=\"0\" showautodescription=\"True\" name=\""
+                            + elementName + "\">\n");
+            xmlBuilder.append(
+                    "<ref name=\"master_template\">/storage/shows/{40F77AF2-AC11-4596-A12B-CA0F3BCE3E8C}/mastertemplates/"
+                            + kunci + "</ref>\n")
+                    .append("\n");
             xmlBuilder.append("<entry name=\"data\">\n").append("\n");
-            for (int j = 1; j <= data.length ; j++) {
+            for (int j = 1; j <= data.length; j++) {
                 xmlBuilder.append("<entry name=\"" + 0 + j + "\">" + data[j - 1] + "</entry>\n").append("\n");
             }
             xmlBuilder.append("</entry>\n");
