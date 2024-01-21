3426a4972368ad5f6f73d04cb9e069bb0addce20
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2024-01-21 19:10:38 +0700

    tambah idtv dan btv channel (masuih di comment)

--- src/main/java/com/cg/ServiceXml.java
+++ src/main/java/com/cg/ServiceXml.java
@@ -38,11 +38,17 @@ public class ServiceXml {
 
     private static class DataEntry {
         private String nama;
+        // private String channel;
         private String key;
         private String[] data;
 
-        public DataEntry(String nama, String key, String[] data) {
+        public DataEntry(String nama,
+                // String channel,
+                String key,
+                String[] data) {
+
             this.nama = nama;
+            // this.channel = channel;
             this.key = key;
             this.data = data;
         }
@@ -50,6 +56,10 @@ public class ServiceXml {
         public String getNama() {
             return nama;
         }
+
+        // public String getChannel() {
+        // return channel;
+        // }
 
         public String getKey() {
             return key;
@@ -71,11 +81,15 @@ public class ServiceXml {
 
     @PostMapping("/saveData")
     public ResponseEntity<Object> saveData(@RequestParam("nama") String nama,
+            @RequestParam("channel") String channel,
             @RequestParam("key") String key,
             @RequestParam("data") String[] data) {
 
         Map<String, Object> responseData = new HashMap<>();
-        dataMap.add(new DataEntry(nama, key, data));
+        dataMap.add(new DataEntry(nama,
+                // channel,
+                key,
+                data));
         // saveXmlToFile(dataMap);
         responseData.put("data", dataMap);
         return new ResponseEntity<>(responseData, HttpStatus.OK);
@@ -108,28 +122,37 @@ public class ServiceXml {
     }
 
     // private void saveXmlToFile(List<DataEntry> dataMap2) {
-    //     String xmlResult = generateXmlFromMap(dataMap2);
-    //     // String filePath = "C:/Users/fadhl/OneDrive/Documents/result.xml";
-    //     String nm = dataMap2.get(0).getNama();
-    //     String nama = nm.replace(" ", "_");
-    //     try (FileWriter fileWriter = new FileWriter(env.getProperty("URL.FILE_IN") + nama + ".xml")) {
-    //         fileWriter.write(xmlResult);
-    //         System.out.println("XML saved to file: " + env.getProperty("URL.FILE_IN") + nama + ".xml");
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
+    // String xmlResult = generateXmlFromMap(dataMap2);
+    // // String filePath = "C:/Users/fadhl/OneDrive/Documents/result.xml";
+    // String nm = dataMap2.get(0).getNama();
+    // String nama = nm.replace(" ", "_");
+    // try (FileWriter fileWriter = new FileWriter(env.getProperty("URL.FILE_IN") +
+    // nama + ".xml")) {
+    // fileWriter.write(xmlResult);
+    // System.out.println("XML saved to file: " + env.getProperty("URL.FILE_IN") +
+    // nama + ".xml");
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
     // }
 
     private String generateXmlFromMap(List<DataEntry> dataMap2) {
         StringBuilder xmlBuilder = new StringBuilder();
         Integer tigak = 3000;
+        // String cnl = dataMap.get(0).getChannel();
+        String btv = "{156ACC91-A335-481B-AD9C-40353AC0A827}";
+        String idtv = "";
 
         xmlBuilder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
         xmlBuilder.append("<archive version=\"1.0\" creator=\"trio\" creator_version=\"2.11.2 (Build 15185)\">\n");
         xmlBuilder.append("<vdom>\n");
         xmlBuilder.append("<entry name=\"storage\">\n");
         xmlBuilder.append("<entry name=\"show\">\n");
-        xmlBuilder.append("<entry name=\"{40F77AF2-AC11-4596-A12B-CA0F3BCE3E8C}\">\n");
+        // if (cnl.equals("BTV")) {
+            xmlBuilder.append("<entry name=\"" + btv + "\">\n");
+        // } else {
+            // xmlBuilder.append("<entry name=\"" + idtv + "\">\n");
+        // }
 
         for (int i = 1; i <= dataMap2.size(); i++) {
 
@@ -139,10 +162,17 @@ public class ServiceXml {
             xmlBuilder.append(
                     "<element loaded=\"0.00\" description=\"\" available=\"1.00\" layer=\"\" templatedescription=\"\" take_count=\"0\" showautodescription=\"True\" name=\""
                             + elementName + "\">\n");
-            xmlBuilder.append(
-                    "<ref name=\"master_template\">/storage/shows/{40F77AF2-AC11-4596-A12B-CA0F3BCE3E8C}/mastertemplates/"
-                            + kunci + "</ref>\n")
-                    .append("\n");
+            // if (cnl.equals("BTV")) {
+                xmlBuilder.append(
+                        "<ref name=\"master_template\">/storage/shows/" + btv + "/mastertemplates/"
+                                + kunci + "</ref>\n")
+                        .append("\n");
+            // } else {
+            //     xmlBuilder.append(
+            //             "<ref name=\"master_template\">/storage/shows/" + idtv + "/mastertemplates/"
+            //                     + kunci + "</ref>\n")
+            //             .append("\n");
+            // }
             xmlBuilder.append("<entry name=\"data\">\n").append("\n");
             for (int j = 1; j <= data.length; j++) {
                 xmlBuilder.append("<entry name=\"" + 0 + j + "\">" + data[j - 1] + "</entry>\n").append("\n");
